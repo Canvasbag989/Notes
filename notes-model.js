6118edbf4f051064bafc1fc08d3e4655c939efd8
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-09-30 18:11:04 +0100

    Add read function

--- notes-model.js
+++ notes-model.js
@@ -1,7 +1,11 @@
 (function(exports) {
-   function Notes(text) {
-     this.text = text;
-   };
+  function Notes(text) {
+    this.text = text;
+  };
 
-     exports.Notes = Notes;
-   }) (this);
+  exports.Notes = Notes;
+}) (this);
+
+Notes.prototype.read = function () {
+  return this.text;
+}
