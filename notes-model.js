3b3038e8771d8d828086532e8463e495701818ac
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-10-01 11:59:57 +0100

    minor changes

--- notes-model.js
+++ notes-model.js
@@ -3,17 +3,9 @@
     this.text = text;
   };
 
-<<<<<<< HEAD
      exports.Notes = Notes;
    }) (this);
 
 Notes.prototype.read = function() {
   return this.text
-=======
-  exports.Notes = Notes;
-}) (this);
-
-Notes.prototype.read = function () {
-  return this.text;
->>>>>>> 6118edbf4f051064bafc1fc08d3e4655c939efd8
 }
